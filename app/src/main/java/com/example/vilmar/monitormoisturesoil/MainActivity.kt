07c24430c32332325d7089e8fc9d1110457610c9
Vilmar Bispo Filho <vilmarbf@ciandt.com>
2017-11-21 16:02:28 -0200

    Added some code

--- app/src/main/java/com/example/vilmar/monitormoisturesoil/MainActivity.kt
+++ app/src/main/java/com/example/vilmar/monitormoisturesoil/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.vilmar.monitormoisturesoil
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.physicaloid.lib.Physicaloid
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mValue: TextView
+
+    private lateinit var mPhysicaloid: Physicaloid
+
+    private val mBaudrate: Int = 9600
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+
+    }
+
+    private fun init() {
+        mValue = findViewById(R.id.tv_value_moisture)
+
+        mPhysicaloid = Physicaloid(this)
+        mPhysicaloid.setBaudrate(mBaudrate)
+
+        if (mPhysicaloid.open()) {
+            mPhysicaloid.addReadListener { size ->
+                val buf = ByteArray(size)
+                mPhysicaloid.read(buf, size)
+                mValue.text = buf.toString()
+            }
+        }
     }
 }
