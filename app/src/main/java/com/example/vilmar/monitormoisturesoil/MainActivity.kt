bf585290b229e0eff557a87fb85b944bc169be70
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2017-11-25 00:12:59 -0200

    update some code

--- app/src/main/java/com/example/vilmar/monitormoisturesoil/MainActivity.kt
+++ app/src/main/java/com/example/vilmar/monitormoisturesoil/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.vilmar.monitormoisturesoil
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Button
+import com.example.vilmar.monitormoisturesoil.settings.SettingsActivity
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mValue: TextView
-    private lateinit var mButton: Button
+    private lateinit var mSettingsButton: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +23,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        mValue = findViewById(R.id.tv_value_moisture)
-        mButton = findViewById(R.id.toggleButton)
+        mValue = findViewById(R.id.tv_main_value_moisture)
+        mSettingsButton = findViewById(R.id.ib_main_settings)
+
+        mSettingsButton.setOnClickListener {
+            startActivity(Intent(SettingsActivity.newIntent(this)))
+        }
     }
 
     private fun tvAppend(tv: TextView, text: CharSequence) {
