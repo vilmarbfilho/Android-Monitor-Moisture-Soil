8be95221a5fc5444dfbcf05c4cef45d0bf8e4124
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2017-11-26 21:38:27 -0200

    Added a listener

--- app/src/main/java/com/example/vilmar/monitormoisturesoil/settings/adapter/USBDeviceAdapter.kt
+++ app/src/main/java/com/example/vilmar/monitormoisturesoil/settings/adapter/USBDeviceAdapter.kt
@@ -15,9 +15,9 @@ import com.hoho.android.usbserial.util.HexDump
 /**
  * Created by vilmar on 24/11/17.
  */
-    class USBDeviceAdapter(private val usbSerialPortList: ArrayList<UsbSerialPort>) : RecyclerView.Adapter<USBDeviceAdapter.DeviceViewHolder>() {
+    class USBDeviceAdapter(private val usbSerialPortList: ArrayList<UsbSerialPort>, val listener: USBDeviceOnClick) : RecyclerView.Adapter<USBDeviceAdapter.DeviceViewHolder>() {
 
-    override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) = holder.bind(usbSerialPortList[position])
+    override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) = holder.bind(usbSerialPortList[position], listener)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceViewHolder =
             DeviceViewHolder(parent.inflate(R.layout.item_device))
@@ -30,20 +30,22 @@ import com.hoho.android.usbserial.util.HexDump
         var card : CardView = v.findViewById(R.id.cv_deviceadapter_container)
         var detail : TextView = v.findViewById(R.id.tv_deviceadapter_detail)
 
-        init {
-            card.setOnClickListener {
-
-            }
-        }
-
-        fun bind(usbSerialPort: UsbSerialPort) {
+        fun bind(usbSerialPort: UsbSerialPort, listener: USBDeviceOnClick) {
 
             val driver: UsbSerialDriver = usbSerialPort.driver
             val device: UsbDevice = driver.device
 
             detail.text = "Vendor ${HexDump.toHexString(device.vendorId.toShort())} - Product ${HexDump.toHexString(device.productId.toShort())}"
+
+            card.setOnClickListener {
+                listener.onClickDevice(usbSerialPort)
+            }
         }
 
+    }
+
+    interface USBDeviceOnClick {
+        fun onClickDevice(usbSerialPort: UsbSerialPort)
     }
 
 }
