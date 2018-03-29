c22f737c9c9e6fc00f1e6c868d5e9fa196e94cd8
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2018-03-29 15:57:14 -0300

    Refactor adapter

--- app/src/main/java/com/example/vilmar/monitormoisturesoil/settings/adapter/USBDeviceAdapter.kt
+++ app/src/main/java/com/example/vilmar/monitormoisturesoil/settings/adapter/USBDeviceAdapter.kt
@@ -1,47 +1,40 @@
 package com.example.vilmar.monitormoisturesoil.settings.adapter
 
 import android.hardware.usb.UsbDevice
-import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.example.vilmar.monitormoisturesoil.R
 import com.example.vilmar.monitormoisturesoil.inflate
 import com.hoho.android.usbserial.driver.UsbSerialDriver
 import com.hoho.android.usbserial.driver.UsbSerialPort
 import com.hoho.android.usbserial.util.HexDump
+import kotlinx.android.synthetic.main.item_device.view.*
 
 /**
  * Created by vilmar on 24/11/17.
  */
-    class USBDeviceAdapter(private val usbSerialPortList: ArrayList<UsbSerialPort>, val listener: USBDeviceOnClick) : RecyclerView.Adapter<USBDeviceAdapter.DeviceViewHolder>() {
+class USBDeviceAdapter(private val usbSerialPortList: ArrayList<UsbSerialPort>, private val listener: USBDeviceOnClick) : RecyclerView.Adapter<USBDeviceAdapter.ViewHolder>() {
 
-    override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) = holder.bind(usbSerialPortList[position], listener)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent.inflate(R.layout.item_device))
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceViewHolder =
-            DeviceViewHolder(parent.inflate(R.layout.item_device))
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(usbSerialPortList[position], listener)
 
-    override fun getItemCount(): Int = usbSerialPortList.size
+    override fun getItemCount() = usbSerialPortList.size
 
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    class DeviceViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-
-        var card : CardView = v.findViewById(R.id.cv_deviceadapter_container)
-        var detail : TextView = v.findViewById(R.id.tv_deviceadapter_detail)
-
-        fun bind(usbSerialPort: UsbSerialPort, listener: USBDeviceOnClick) {
+        fun bind(usbSerialPort: UsbSerialPort, listener: USBDeviceOnClick) = with(itemView){
 
             val driver: UsbSerialDriver = usbSerialPort.driver
             val device: UsbDevice = driver.device
 
-            detail.text = "Vendor ${HexDump.toHexString(device.vendorId.toShort())} - Product ${HexDump.toHexString(device.productId.toShort())}"
+            tvDeviceadapterDetail.text = "Vendor ${HexDump.toHexString(device.vendorId.toShort())} - Product ${HexDump.toHexString(device.productId.toShort())}"
 
-            card.setOnClickListener {
+            cvDeviceadapterContainer.setOnClickListener {
                 listener.onClickDevice(usbSerialPort)
             }
         }
-
     }
 
     interface USBDeviceOnClick {
