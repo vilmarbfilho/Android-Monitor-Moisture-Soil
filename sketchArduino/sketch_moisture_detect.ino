758cbd6ae5352f828de2f03b65ae259b50bcaf4a
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2019-02-15 20:35:55 -0200

    Added sketch arduino moisture detect

---
+++ sketchArduino/sketch_moisture_detect.ino
@@ -0,0 +1,33 @@
+#define pin_analogic_signal A0
+
+const int just_dry = 0;
+const int moderate_humidity = 1;
+const int moist_soil = 2;
+
+int value_analogic;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pin_analogic_signal, INPUT);
+}
+
+void loop() {
+  value_analogic = analogRead(pin_analogic_signal);
+
+  if (value_analogic > 0 && value_analogic < 400)
+  {
+    Serial.print(just_dry);
+  }
+
+  if (value_analogic > 400 && value_analogic < 800)
+  {
+    Serial.print(moderate_humidity);
+  }
+
+  if (value_analogic > 800 && value_analogic < 1024)
+  {
+    Serial.print(moist_soil);
+  }
+
+  delay(200);
+}
